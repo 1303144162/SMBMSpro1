5afef87ce5fd132c4299e1654df9ea62016bacdf
1303144162 <1303144162@qq.com>
2022-09-08 05:17:30 +0800

    1.7

--- src/main/java/com/Slzr/servlet/UserToolServlet.java
+++ src/main/java/com/Slzr/servlet/UserToolServlet.java
@@ -21,7 +21,7 @@ import java.util.Map;
 
 @WebServlet(name="UserToolServlet",value = "/jsp/user.do")
 public class UserToolServlet extends HttpServlet {
-    //复用
+    //复用方法和正常建立一个Servlet类的操作基本相同，不同点是原来在doGet类中写的具体逻辑代码写在了updatePwd类中，仅仅在doGet中调用这个方法
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
             String method=req.getParameter("method");
@@ -37,11 +37,12 @@ public class UserToolServlet extends HttpServlet {
         doGet(req, resp);
     }
 
-    public void pwdmodify(HttpServletRequest req,HttpServletResponse resp) throws IOException {
-        Object usersession= req.getSession().getAttribute(Constants.User_Session);
-       String  oldpassword=req.getParameter("oldpassword");
-        Map<String, String> resultMap = new HashMap<String, String>();
-            String userpwd=((User)usersession).getUserPassword();
+    public void pwdmodify(HttpServletRequest req,HttpServletResponse resp) throws IOException {//ajax判断原密码
+        Object usersession= req.getSession().getAttribute(Constants.User_Session);//获取session
+       String  oldpassword=req.getParameter("oldpassword");//获取密码
+
+        Map<String, String> resultMap = new HashMap<String, String>();//map集合
+            String userpwd=((User)usersession).getUserPassword();//Session中用户的密码
             if(userpwd.equals(oldpassword)) {
                 resultMap.put("result", "true");
             }else{
@@ -50,6 +51,7 @@ public class UserToolServlet extends HttpServlet {
         }
         resp.setContentType("application/json");
         PrintWriter pw=resp.getWriter();
+        //把resultMap保存的键值对转换为json格式的键值对
         pw.write(JSONArray.toJSONString(resultMap));
         pw.flush();
         pw.close();
